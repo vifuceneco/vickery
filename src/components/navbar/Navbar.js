15c2a5123962e32d47d3cdec147aae36bb631b62
Victoria Fuceneco <fuceneco.vi@gmail.com>
2021-10-12 23:40:51 -0300

    create navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import './Navbar.css'
+
+export const Navbar = () => {
+    return (
+        <div className="NavBar">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Recetas</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </div>
+    )
+}
